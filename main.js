8a0caeed9f6ee6c18bc1bbd3cdef1994f12d7834
styveprestes <160791985+styveprestes@users.noreply.github.com>
2025-05-28 21:24:34 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,8 +15,8 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo1 = new Date("2026-01-01T00:00:00");
+const tempoObjetivo2 = new Date("2025-05-28T00:22:00");
 const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
